48a6993203ca8d70f98ca8e1a7cf7a9a8e3992d2
FalloutX <inderjitsidhu@outlook.com>
2016-09-01 22:48:12 +0530

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,30 @@
+new Vue({
+  el: '#app',
+  data: {
+    message: 'Learning Vue.js'
+  }
+});
+
+new Vue({
+  el: '#todo-list',
+  data: {
+    newTodo: '',
+    todos: [
+      {text: 'Learning Vue.js'},
+      {text: 'Build Something!'},
+      {text: 'It could be good!'}
+    ]
+  },
+  methods: {
+    addTodo: function addTodo () {
+      var text = this.newTodo.trim();
+      if(text) {
+        this.todos.push({text: text});
+        this.newTodo = '';
+      }
+    },
+    removeTodo: function removeTodo(index) {
+      this.todos.splice(index, 1);
+    }
+  }
+});
