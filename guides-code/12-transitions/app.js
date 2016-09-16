29a3ea3544620651fe7c2c5ff09d3883b89e7458
FalloutX <inderjitsidhu@outlook.com>
2016-09-16 11:11:03 +0530

    Add Basic Transitions code. Don't use titles inside link tags, they have special meaning.

---
+++ guides-code/12-transitions/app.js
@@ -0,0 +1,41 @@
+
+
+const vm  = new Vue({
+  el: '#app',
+  data :{
+    show: false
+  },
+  methods: {
+    toggleShow: function(){
+      this.show = !this.show;
+    }
+  }
+});
+
+// Javascript hooks for 'expand' transition. 
+Vue.transition('expand', {
+  beforeEnter: function (el) {
+    el.textContent = 'beforeEnter'
+  },
+  enter: function (el) {
+    el.textContent = 'enter'
+  },
+  afterEnter: function (el) {
+    el.textContent = 'afterEnter'
+  },
+  enterCancelled: function (el) {
+    // handle cancellation
+  },
+  beforeLeave: function (el) {
+    el.textContent = 'beforeLeave'
+  },
+  leave: function (el) {
+    el.textContent = 'leave'
+  },
+  afterLeave: function (el) {
+    el.textContent = 'afterLeave'
+  },
+  leaveCancelled: function (el) {
+    // handle cancellation
+  }
+})
